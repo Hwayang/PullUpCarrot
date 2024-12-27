cf7c4b02eee452347cee4b6383bc2350496802c4
Hwayang <hwayanggames@gmail.com>
2024-12-27 15:26:46 +0900

    [Update] 당근 위로 이동

--- PullUpCarrot/Assets/01. Scripits/Carrot.cs
+++ PullUpCarrot/Assets/01. Scripits/Carrot.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Carrot : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPos = transform.position;
+
+        if(Input.GetMouseButton(0))
+        {
+            currentPos.y += speed;
+        }
+
+        this.transform.position = currentPos;
     }
 }
