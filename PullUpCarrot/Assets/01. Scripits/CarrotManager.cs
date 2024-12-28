5df580e7c253f2f71ae20b0fc41858dfc36de36b
Hwayang <hwayanggames@gmail.com>
2024-12-28 17:31:48 +0900

    [Update] 당근 뽑는 방식 드래그로 수정

--- PullUpCarrot/Assets/01. Scripits/CarrotManager.cs
+++ PullUpCarrot/Assets/01. Scripits/CarrotManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UIElements;
 using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine.LightTransport;
 
 public class CarrotManager : MonoBehaviour
 {
@@ -34,13 +36,25 @@ public class CarrotManager : MonoBehaviour
     [UnityEngine.Range(0, 0.05f)]
     private float downSpeed;
 
+    // 드래그 시작 시, 드래그된 당근(GameObject)
+    private GameObject draggedCarrot = null;
+    
+    // 드래그 시작 시점에 “각 당근의 위치”를 기록해둘 리스트
+    private List<Vector3> initialPositions = new List<Vector3>();
+
+    private Vector3 draggedCarrotStartPos;
+
     uint carrotCount = 0;
-    bool alreadySummoned = false;
+
+    //마우스 드래그 여부
+    private bool isMouseDrag = false;
+
+    // 드래그된 당근의 마우스 클릭 offset
+    private Vector3 dragOffset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         // 새로 만든 오브젝트를 변수에 담는다
         GameObject newCarrot = Instantiate(carrotTop, new Vector2(0, 0), Quaternion.identity);
 
@@ -50,27 +64,75 @@ public class CarrotManager : MonoBehaviour
         carrotCount++;
     }
 
+    private Vector2 GetMouseWorldPosition()
+    {
+        Vector2 screenPos = Input.mousePosition;
+
+        return Camera.main.ScreenToWorldPoint(screenPos);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            foreach (GameObject carrot in carrotList)
+
+            float maxDistance = 10;
+
+            Debug.DrawRay(carrotSenser.transform.position, Vector2.down * maxDistance, Color.red);
+
+            // Raycast로 클릭된 오브젝트 확인
+            RaycastHit2D hit = Physics2D.Raycast(carrotSenser.transform.position, Vector2.down, 10, LayerMask.GetMask("Object"));
+
+            if (hit.collider)
             {
-                Vector2 currentPos = carrot.transform.position;
-                currentPos.y += upSpeed;
-                carrot.transform.position = currentPos;
+                // 만약 클릭 대상이 당근 리스트에 있는 객체라면 드래그 시작
+                if (carrotList.Contains(hit.collider.gameObject))
+                {
+                    draggedCarrot = hit.collider.gameObject;
+                    isMouseDrag = true;
+
+                    // 모든 당근의 현재 위치를 기록
+                    initialPositions.Clear();
+                    foreach (var carrot in carrotList)
+                    {
+                        initialPositions.Add(carrot.transform.position);
+                    }
+
+                    // 드래그된 당근의 “드래그 시작 위치”
+                    draggedCarrotStartPos = draggedCarrot.transform.position;
+
+                    // 마우스와 당근 사이의 offset
+                    Vector3 mousePos = GetMouseWorldPosition();
+                    dragOffset = draggedCarrotStartPos - mousePos;
+                }
             }
         }
-        else
+        // 2) 드래그 중
+        else if (Input.GetMouseButton(0) && isMouseDrag && draggedCarrot != null)
         {
-            foreach (GameObject carrot in carrotList)
+            Vector3 mousePos = GetMouseWorldPosition();
+
+            // draggedCarrot의 새 위치
+            Vector3 newCarrotPos = mousePos + dragOffset;
+
+            // 당근이 얼마나 이동했는지 (Delta)
+            Vector3 delta = newCarrotPos - draggedCarrotStartPos;
+
+            // 모든 당근 이동
+            for (int i = 0; i < carrotList.Count; i++)
             {
-                Vector2 currentPos = carrot.transform.position;
-                currentPos.y -= downSpeed;
-                carrot.transform.position = currentPos;
+                // initialPositions[i]에서 delta만큼 이동
+                carrotList[i].transform.position = initialPositions[i] + delta;
             }
         }
+        // 3) 마우스를 뗐을 때
+        else if (Input.GetMouseButtonUp(0) && isMouseDrag)
+        {
+            isMouseDrag = false;
+            draggedCarrot = null;
+        }
+
 
         if (carrotSenser.isCarrotSummon && carrotCount <= maxCarrotCount)
         {
