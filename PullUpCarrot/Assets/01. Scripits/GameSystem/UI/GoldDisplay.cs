baf8bccf4ebb299297cf891e11b14b37ed5f20bb
Hwayang <hwayanggames@gmail.com>
2025-01-06 18:32:11 +0900

    [Update] 소지 골드 UI 구현

---
+++ PullUpCarrot/Assets/01. Scripits/GameSystem/UI/GoldDisplay.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+
+public class GoldDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text goldText;
+
+    private void Update()
+    {
+        // 매 프레임, UserDataManager의 currentGold를 가져와서 표시
+        goldText.text = "Gold: " + UserData.Instance.currentGold.ToString();
+    }
+}
